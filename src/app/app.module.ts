1421dcf440a35cf05051416e801c66eb206e1938
Tony Tom K <tony.tomk@thermofisher.com>
2018-06-19 18:23:49 +0530

    Forms
    Forms- validations, Template driven approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
